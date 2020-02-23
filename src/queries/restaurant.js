3fcab690d3eb99502f4f5415a29f8a8d19fbeb73
David Krasniy <davidkrasniy@gmail.com>
2020-02-23 09:55:42 -0800

    home

--- src/queries/restaurant.js
+++ src/queries/restaurant.js
@@ -16,13 +16,35 @@ export const GET_RESTAURANT_DETAILS_FOR_USER = gql`
           restaurant_id
           id
           date
+          feedcount
           comments
+          userclaimer {
+            id
+          }
         }
         allEvents: events(order_by: {date: desc}) {
           restaurant_id
           id
           date
           comments
+          feedcount
+          userclaimer {
+            id
+          }
+          restauranteventid {
+            name
+            address
+          }
+        }
+        myClaimed: events(where: {claimer_id: {_eq: $userId}}) {
+          restaurant_id
+          id
+          date
+          comments
+          feedcount
+          userclaimer {
+            id
+          }
           restauranteventid {
             name
             address
@@ -33,8 +55,19 @@ export const GET_RESTAURANT_DETAILS_FOR_USER = gql`
 
 
 export const CREATE_EVENT = gql`
-  mutation CreateEvent($comments: String!, $restaurant_id: Int!, $user_id: Int!) {
-    insert_events(objects: {comments: $comments, restaurant_id: $restaurant_id, user_id:  $user_id}) {
+  mutation CreateEvent($comments: String!, $feedCount: Int!, $restaurant_id: Int!, $user_id: Int!) {
+    insert_events(objects: {comments: $comments, feedcount:$feedCount, restaurant_id: $restaurant_id, user_id:  $user_id}) {
+      returning {
+        id
+      }
+    }
+  }
+`;
+
+
+export const UPDATE_EVENT = gql`
+  mutation updateEventWithClaimer($eventId: Int!, $claimerId: Int) {
+    update_events(where: {id: {_eq: $eventId}}, _set: {claimer_id: $claimerId}) {
       returning {
         id
       }
